9a20a26d3913ba9ccc1580e98c03251eeec14bf7
MdRifatchimu <himu2411@swfu.edu.cn>
2021-08-06 19:30:20 +0600

    saving practice in github

---
+++ interest-calculator.js
@@ -0,0 +1,10 @@
+const principal = 100;
+const time = 1;
+const rate = .05;
+const n = 12;
+const compoundInterest = (p, t, r, n) => {
+   const amount = p * (Math.pow((1 + (r / n)), (n * t)));
+   const interest = amount - p;
+   return interest;
+};
+console.log(compoundInterest(principal, time, rate, n));
